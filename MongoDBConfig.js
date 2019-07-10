5e7e7c95286a73f30156d8d2fd05faf24c6bae9b
daoxiaonoodles <bit2008wang@163.com>
2019-07-10 08:38:57 +0800

    Add files via upload

---
+++ MongoDBConfig.js
@@ -0,0 +1,11 @@
+/**
+ * MongoDB数据库配置信息
+ */
+module.exports = {
+	url: 'mongodb://root:root@localhost:27017/',
+	db:'puppeteer',
+	coverage_collection:'coverage',
+	interface_collection:'interface',
+	interfaceExec_collection:'interfaceExec',
+	demo_collection:'demo'
+};
