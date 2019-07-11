0643424cbce02cd99d0920a44a1cb2277211fabd
yangshuwei666 <524122401@qq.com>
2019-07-11 11:00:12 +0800

    Update PlaybackConfig.js
    example

--- PlaybackConfig.js
+++ PlaybackConfig.js
@@ -2,26 +2,19 @@
  * 配置信息
  */
 module.exports = {
-	'host': '192.168.23.38:8080',
+	'host': 'XXX.XXX.XX.XX:8080',
 	'port': '8080',
-	'host_name': '192.168.23.38',
+	'host_name': 'XXX.XXX.XX.38',
 
 	'playback_testcase_id_or': {
 		"$or": [
-			//{"testcaseId": 'aosplus-interface-001'},
-			{"testcaseId": 'aosplus-interface-004'},
-			//{"testcaseId":'aisino-OperationManager-001'},
+			//{"testcaseId": 'example-interface-001'},
+			{"testcaseId": 'example-interface-004'},
+			//{"testcaseId":'example-OperationManager-001'},
 		]
 	},
 
-	/*'playback_testcase_id_in': {//比or慢
-		"testcaseId" : {
-			"$in": [
-				//'aosplus-interface-001',
-				'aosplus-interface-002'
-			]
-		}
-	},*/
+
 
 	'replace_key': [
 		'cGuid',
